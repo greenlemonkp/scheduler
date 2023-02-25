92969e737b0c5cf3087fc4fe3002afd95f565835
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2023-02-25 11:51:55 -0500

    Add update spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -29,6 +29,29 @@ export default function useApplicationData() {
 
   const setDay = (day) => setState({ ...state, day });
 
+  // to update spots when save
+  const updateSpotsOnSave = (appointment, id) => {
+    if (
+      state.appointments[id].interview === null &&
+      appointment.interview !== null
+    ) {
+      const currentDay = state.days.find((day) => day.name === state.day);
+      currentDay.spots--;
+    }
+  };
+
+  // to update spots when delete
+  const updateSpotsOnDelete = (appointment, id) => {
+    if (
+      state.appointments[id].interview !== null &&
+      appointment.interview === null
+    ) {
+      const currentDay = state.days.find((day) => day.name === state.day);
+      currentDay.spots++;
+    }
+  };
+
+  // book new interview
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -38,12 +61,14 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
+    updateSpotsOnSave(appointment, id);
 
     return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
       setState((prev) => ({ ...prev, appointments }));
     });
   }
 
+  // delete already existing interview
   function cancelInterview(id) {
     const appointment = {
       ...state.appointments[id],
@@ -53,6 +78,8 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
+
+    updateSpotsOnDelete(appointment, id);
     return axios.delete(`/api/appointments/${appointment.id}`).then(() => {
       setState({ ...state, appointments });
     });

b73052d9a430f4c1cb2fd90011299b1691c0a6ec
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2023-02-27 09:48:54 -0500

    Add integration testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,6 +6,7 @@ import {
   waitForElement,
   fireEvent,
   getByText,
+  getAllByTestId,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -20,5 +21,21 @@ describe("Application", () => {
     fireEvent.click(getByText("Tuesday"));
 
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
   });
 });
